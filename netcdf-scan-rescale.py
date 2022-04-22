8bf8e2b6b81dd921f95a0b49d96fa92ff7c84381
Percy <pyzahl@ymail.com>
2022-04-22 12:14:00 -0400

    Added lookup rescale

--- netcdf-scan-rescale.py
+++ netcdf-scan-rescale.py
@@ -1,9 +1,9 @@
 import netCDF4 as nc
-### WARNING: THIS INSTANTLY RESCALES THE SCAN DIMESNIONS
-### NOTE: IT DOES NOT RESCALE THE XY-LOOKUPs yet! (ToDo)
+import numpy as np
 
 ## factors to rescale - - see below
-xy_rescale_by = 1.0
+x_rescale_by = 1.0
+y_rescale_by = 1.0
 z_rescale_by = 1.0
 
 ch=0
@@ -23,12 +23,15 @@ sc = nc.Dataset(filename= ncfile, mode="r+")
 print ('NCfile dx,dy,dz: ', sc['dx'][0], sc['dy'][0], sc['dz'][0])
 print ('NCfile range x,y: ', sc['rangex'][0], sc['rangey'][0])
 
+#print('NCfile dimx: ', sc['dimx'][:])
+#print('NCfile dimx: ', sc['dimy'][:])
+
 ## rescale dx,dy
 
-tmp = sc['dx'][0] *  xy_rescale_by
+tmp = sc['dx'][0] *  x_rescale_by
 sc['dx'][0] = tmp
 
-tmp = sc['dy'][0] *  xy_rescale_by
+tmp = sc['dy'][0] *  y_rescale_by
 sc['dy'][0] = tmp
 
 
@@ -38,16 +41,26 @@ sc['dz'][0] = tmp
 
 
 ## must also apply to ranges to match:
-tmp = sc['rangex'][0] *  xy_rescale_by
+tmp = sc['rangex'][0] *  x_rescale_by
 sc['rangex'][0] = tmp
 
-tmp = sc['rangey'][0] *  xy_rescale_by
+tmp = sc['rangey'][0] *  y_rescale_by
 sc['rangey'][0] = tmp
+
+xlookup = sc['dimx'][:]
+ylookup = sc['dimy'][:]
+sc['dimx'][:] = xlookup*  x_rescale_by
+sc['dimy'][:] = ylookup*  y_rescale_by
+#print('NCfile dimx: ', sc['dimx'][:])
+#print('NCfile dimx: ', sc['dimy'][:])
+
 
 print('------ final geometry as updated -----------')
 
 print ('NCfile dx,dy,dz: ', sc['dx'][0], sc['dy'][0], sc['dz'][0])
 print ('NCfile range x,y: ', sc['rangex'][0], sc['rangey'][0])
+
+
 
 sc.close ()
 
